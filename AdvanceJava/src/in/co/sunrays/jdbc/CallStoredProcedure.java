831e9592b39a514fe87d585a956acc5eb59e6255
mahi0130 <144420768+mahi0130@users.noreply.github.com>
2024-09-05 11:18:42 +0530

    Update CallStoredProcedure.java

--- AdvanceJava/src/in/co/sunrays/jdbc/CallStoredProcedure.java
+++ AdvanceJava/src/in/co/sunrays/jdbc/CallStoredProcedure.java
@@ -5,15 +5,31 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Types;
 
-/*
- * copyright (c) sunRays Technologies Indore
- * @author: sunRays Developer
- * @url : www.sunrays.co.in
- *
- */
-
 /**
- * Tests Stored Procedure call.
+ * Demonstrates how to call a stored procedure in a MySQL database using JDBC.
+ * 
+ * This program establishes a connection to a MySQL database, calls a stored 
+ * procedure using a `CallableStatement`, and retrieves the result via an 
+ * output parameter. It handles a stored procedure that returns an integer 
+ * value.
+ * 
+ * Steps:
+ * 1. Load the MySQL JDBC driver.
+ * 2. Establish a connection to the MySQL database.
+ * 3. Use a `CallableStatement` to call the stored procedure.
+ * 4. Register the output parameter to capture the integer result.
+ * 5. Execute the callable statement and retrieve the output.
+ * 
+ * The stored procedure `ProTest(?)` is expected to return an integer value, 
+ * which is printed to the console.
+ * 
+ * @author SunRays Developer
+ * @version 1.0
+ * @since 2023
+ * @see java.sql.Connection
+ * @see java.sql.DriverManager
+ * @see java.sql.CallableStatement
+ * @see java.sql.Types
  * 
  * @Copyright (c) SunilOS. All rights reserved.
  * @URL www.SunilOS.com
@@ -21,24 +37,19 @@ import java.sql.Types;
 
 public class CallStoredProcedure {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		Class.forName("com.mysql.jdbc.Driver").newInstance();
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
 
-		Connection conn = DriverManager.getConnection(
-				"jdbc:mysql://localhost/ST_TEST", "root", "root");
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost/ST_TEST", "root", "root");
 
-		CallableStatement callStmt = conn.prepareCall("{CALL ProTest(?)}");
+        CallableStatement callStmt = conn.prepareCall("{CALL ProTest(?)}");
 
-		callStmt.registerOutParameter(1, Types.INTEGER);
+        callStmt.registerOutParameter(1, Types.INTEGER);
 
-		callStmt.execute();
+        callStmt.execute();
 
-		System.out.println(" Count " + callStmt.getInt(1));
-
-	}
-
+        System.out.println(" Count " + callStmt.getInt(1));
+    }
 }
